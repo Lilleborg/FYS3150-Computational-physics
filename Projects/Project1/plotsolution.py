05df202b3f0ad128847c0d0fcf35afbcc2b0ed55
Jakob Borg <borg.jakob@gmail.com>
2018-09-06 19:47:38 +0200

    Update main.cpp and .pro with optimalisation and started LU-decomp

--- Projects/Project1/plotsolution.py
+++ Projects/Project1/plotsolution.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-test = "generalmethod"
 def exact_sol(x):
 	return 1-(1-np.exp(-10.0))*x-np.exp(-10.0*x)
 
-for k in range(1,4):
+test = "gauss_elim_"
+plt.figure(1)
+for k in range(1,3):
 	try:
 		numerical = np.loadtxt("build/"+test+str(k)+"sol.dat")
 		x = np.loadtxt("build/"+test+str(k)+"points.dat")
@@ -14,10 +15,28 @@ for k in range(1,4):
 		exit()
 
 	plt.plot(x,numerical,label= "Numerical, n= "+str(10**k))
+LU = np.loadtxt("build/LU_decompsol.dat")
+plt.plot(x,LU,"ro",label="LU")
 exact = exact_sol(x)
 plt.plot(x,exact,"r--",label="Exact")
 plt.xlabel("$x$")
 plt.ylabel("$u(x)$")
-plt.title("Numerical solution with varying number of points of the Poisson equation")
+plt.title("General numerical solution with varying number of points of the Poisson equation")
 plt.legend()
+
+# plt.figure(2)
+# for k in range(1,4):
+# 	try:
+# 		numerical = np.loadtxt("build/"+test+str(k)+"simpl"+"sol.dat")
+# 		x = np.loadtxt("build/"+test+str(k)+"simpl"+"points.dat")
+# 	except:
+# 		print "Failed in loading datafiles"
+# 		exit()
+
+# 	plt.plot(x,numerical,label= "Numerical, n= "+str(10**k))
+# plt.plot(x,exact,"r--",label="Exact")
+# plt.xlabel("$x$")
+# plt.ylabel("$u(x)$")
+# plt.title("Special numerical solution with varying number of points of the Poisson equation")
+# plt.legend()
 plt.show()
--- Projects/Project1/project_main/main.cpp
+++ Projects/Project1/project_main/main.cpp
@@ -2,6 +2,7 @@
 #include <armadillo>
 #include <cmath>
 #include <string>
+#include "time.h"
 
 
 using namespace std;
@@ -10,14 +11,18 @@ using namespace arma;
 // Functions
 inline double f(double x) {return 100.0*exp(-10.0*x);}
 inline double exact_sol(double x) {return 1-(1-exp(-10.0))*x-exp(-10.0*x);}
-void forw_backw(uword n,vec &a,vec &b,vec &c,vec &f,vec &sol);
+void gauss_general(uword n,vec &a,vec &b,vec &c,vec &f,vec &sol);
 void save_arrays(string filename,uword n, vec datapoints, vec solution);
+void gauss_specific(int n, vec &b, vec &f, vec &u);
+void arma_lu(int n,int a,int b,int c,vec f,vec datapoints);
 
 int main(int argc, char *argv[])
 {
     string outfilename;
     int exponent;
-
+    clock_t start, finish;
+    clock_t starttotal,finishtotal;
+    starttotal = clock();
     // Bad ussage test
     if(argc!=3){
         printf("Bad ussage bro; want [number of grid points in base 10, 10^n; data text file name]\n");
@@ -33,12 +38,15 @@ int main(int argc, char *argv[])
         string fileout = outfilename;
         string argument = to_string(i);
         fileout.append(argument);
+        string fileoutsimple = fileout;
+        fileoutsimple.append(string("simpl"));
 
         // Allocate memory / initialize arrays
         double h = 1.0/(n+1); //step size
         double hh = h*h;
         vec a(nlong-1,fill::ones),b(nlong,fill::ones),c(nlong-1,fill::ones); //diagonals
         vec sol(nlong,fill::zeros),x(nlong,fill::zeros),f_til(nlong);
+        vec b_simpl(nlong,fill::zeros),f_til_simpl(nlong,fill::zeros),u_simpl(nlong,fill::zeros);
 
         // Fill/shape arrays
         a = -1*a; c = -1*c; b = 2*b;
@@ -46,28 +54,59 @@ int main(int argc, char *argv[])
         for (uword j = 0; j < nlong; ++j) {
             x(j) = (j+1)*h;
             f_til(j) = hh*f(x(j));
-//            if ( i == exponent){
-//                // Only compute and save most accurate exact solution
-//                vec exactv(nlong);
-//                exactv(j) = exact_sol(x(j));
+            b_simpl(j) = (1.0+(j+1))/(double(j+1));
             }
-        forw_backw(nlong,a,b,c,f_til,sol);
-        save_arrays(fileout,nlong,x,sol);
-        //exactv.save(fileout+"exact.dat",raw_ascii);
+        f_til_simpl = f_til;
+        start = clock();
+        gauss_general(nlong,a,b,c,f_til,sol);
+        finish = clock();
+        double time = double((finish-start))/double(CLOCKS_PER_SEC);
+        //cout << "test" << time << endl;
+        cout << endl;
+        printf("Time used for %dx%d matrix using general method = %.15f\n",n,n,time);
+
+        start = clock();
+        gauss_specific(n,b_simpl,f_til_simpl,u_simpl);
+        finish = clock();
+        time = double((finish-start))/double(CLOCKS_PER_SEC);
+        printf("Time used for %dx%d matrix using specific method = %.15f\n",n,n,time);
+
+        if (i<6){
+            save_arrays(fileout,nlong,x,sol);
+            save_arrays(fileoutsimple,nlong,x,u_simpl);
+        }
+        // LU-decomp
+        arma_lu(n,-1,2,-1,f_til,x);
     }
+
+    finishtotal = clock();
+    double timetotal = double (finishtotal-starttotal)/double(CLOCKS_PER_SEC);
+    printf("Total time = %.15f\n",timetotal);
     return 0;
 }
-void forw_backw(uword n,vec &a,vec &b,vec &c,vec &f,vec &sol){
+void gauss_general(uword n,vec &a,vec &b,vec &c,vec &f,vec &sol){
+    double factor;
     for (uword i = 1; i < n; ++i ){
-        b.at(i) = b.at(i) - (a.at(i-1)*c.at(i-1))/b.at(i-1);
-        f.at(i) = f.at(i) - (a.at(i-1)*f.at(i-1))/b.at(i-1);
+        factor = a[i-1]/b[i-1];
+        b[i] = b[i] - c[i-1]*factor;
+        f[i] = f[i] - factor*f[i-1];
     }
-    sol.at(n-1) = f.at(n-1)/b.at(n-1);
+    sol[n-1] = f[n-1]/b[n-1];
     for (int i = n-2; i >= 0;--i){
-        sol.at(i) = (f.at(i)-c.at(i)*sol.at(i+1))/b.at(i);
+        sol[i] = (f[i]-c[i]*sol[i+1])/b[i];
     }
 
 }
+void gauss_specific(int n, vec &b, vec &f, vec &u){
+    for (int i = 1; i < n; i++){
+        f[i] = f[i] + f[i-1]/b[i-1];
+    }
+    u(n-1) = f(n-1)/b(n-1);
+    for (int i = n-2; i >= 0; --i) {
+        u[i] = (f[i]+u[i+1])/b[i];
+    }
+}
+
 void save_arrays(string filename,uword n, const vec datapoints, const vec solution){
     // Adding endpoints to arrays that are to be saved
     vec outpoints(n+2);vec outsol(n+2);
@@ -81,4 +120,31 @@ void save_arrays(string filename,uword n, const vec datapoints, const vec soluti
     string datafilename = filename + "points.dat";
     outsol.save(solfilename,arma::raw_ascii);
     outpoints.save(datafilename,raw_ascii);
+}
+
+void arma_lu(int n,int a,int b,int c,vec f,vec datapoints)
+{
+    uword nlong = n;
+    mat A(nlong,nlong,fill::eye);
+    mat L(nlong,nlong);
+    mat U(nlong,nlong);
+    vec x(nlong);
+    vec v(nlong);
+    A = b*A;    // Set diagonal to value b
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <n; j++) {
+            if (j==i+1){
+                A(i,j) = c;
+            }
+            if (j==i-1){
+                A(i,j) = a;
+            }
+        }
+    }
+
+    lu(L, U, A);
+    x = solve(L, f);
+    v = solve(U, x);
+    // v is the solution
+    save_arrays("LU_decomp",nlong,datapoints,v);
 }
