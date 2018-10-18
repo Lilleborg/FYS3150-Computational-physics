c96351452a457a97aacd04995c0714a809dfc118
wanjapaulsen <wanjap@student.matnat.uio.no>
2018-10-18 17:26:41 +0200

    Did several changes, will continue trying to solve the problem

--- Projects/Project3/python/3Dplotpos.py
+++ Projects/Project3/python/3Dplotpos.py
@@ -15,20 +15,22 @@ def plotbody(pos,ax):
 
 
 if __name__ == '__main__':
-	positions = np.loadtxt('../buildSSC/positions.txt',unpack=True)
-	nr_objects = 2
+	positions = np.loadtxt('../build/positions_euler.txt', unpack=True)
+	nr_objects = 3
 	
 	sun = positions[:,::nr_objects]
 	earth = positions[:,1::nr_objects]
+	jupiter = positions[:,2::nr_objects]
 	#earth2 = positions[:,2::nr_objects]
 	
 	fig = plt.figure(2)
 	ax = fig.gca(projection='3d')
-	ax.plot(earth[0,:],earth[1,:],earth[2,:])
-	#ax.plot(earth2[0,:],earth2[1,:],earth2[2,:])
-	ax.plot(sun[0,:],sun[1,:],sun[2,:],'ro')
+	ax.plot(earth[0,::100],earth[1,::100],earth[2,::100],label = 'earth')
+	ax.plot(jupiter[0,::100],jupiter[1,::100],jupiter[2,::100],label = 'Jupiter')
+	#ax.plot(earth2[0,::100],earth2[1,::100],earth2[2,::100])
+	ax.plot(sun[0,::100],sun[1,::100],sun[2,::100],'ro')
 	#ax.plot([rvec[0,0]],[rvec[1,0]],[rvec[2,0]],'yo')
-	
+	plt.legend()
 	ax.set_xlabel('x [m]')
 	ax.set_ylabel('y [m]')
 	ax.set_zlabel('z [m]')
