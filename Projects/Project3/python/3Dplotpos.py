abe42c70713667147cd07e514fa2b9bda2b048a9
Jakob Borg <jakobbor@student.matnat.uio.no>
2018-10-23 17:51:09 +0200

    FInalized 3dplot and changes savepath in solarsystem.cpp

--- Projects/Project3/python/3Dplotpos.py
+++ Projects/Project3/python/3Dplotpos.py
@@ -13,7 +13,7 @@ def unpack(solver,exceptions = []):
 			if exept in ele:
 				list_of_files.remove(ele)
 	list_names = [i.strip(path)  for i in list_of_files]
-	list_names = [i.strip("_.txt")  for i in list_names]
+	list_names = [i.strip(".txt")  for i in list_names]
 	list_objects_pos = []
 	for files in list_of_files:
 		print files
@@ -23,17 +23,20 @@ def unpack(solver,exceptions = []):
 if __name__ == '__main__':
 	try:
 		solver = sys.argv[1]
-		exceptions = sys.argv[2:]
 	except IndexError:
-		print 'Please provide solver type and optional exceptions in command line'
-		print 'Solver type not given, using data from Verlet with no exceptions'
+		print 'Solver type not given, first cmd argument. Using "Verlet".'
 		solver = "Verlet"
+	try:
+		plot_filename = sys.argv[2]
+	except IndexError:
+		print 'Filename for saving plots not given, defaulting to "plot_default_'+solver+'.pdf'
+		plot_filename = 'plot_default_' + solver + '.pdf'
+	try :
+		exceptions = sys.argv[3:]
+	except IndexError:
+		print 'No exceptions given.'
 		exceptions = []
-	except ValueError:	
-		print 'User input not understood, using data from Verlet with no exceptions'
-		solver = "Verlet"	
-		exceptions = []
-
+	
 	objects,names = unpack(solver,exceptions)
 	skip = int(len(objects[0][0])/1000.)
 	
@@ -44,21 +47,33 @@ if __name__ == '__main__':
 
 	plt.figure(0)
 	for pos,name in zip(objects,names):
-		plt.plot(pos[0,::skip],pos[1,::skip],label=name)
+		if name == "arth":	#Fixing error from str.strip
+			name = "Earth"
+		if name == "Sun":
+			plt.plot(pos[0,::skip],pos[1,::skip],'o',label=name)	
+		else:
+			plt.plot(pos[0,::skip],pos[1,::skip],label=name)
 	
 	plt.xlabel('x [AU]')
 	plt.ylabel('y [AU]')
 	plt.legend()
+	plt.savefig(plot_filename)
 
 	fig = plt.figure(1)
 	ax = fig.gca(projection='3d')
 	for pos,name in zip(objects,names):
-		ax.plot(pos[0,::skip],pos[1,::skip],pos[2,::skip],label=name)
+		if name == "arth":	#Fixing error from str.strip
+			name = "Earth"
+		if name == "Sun":
+			plt.plot(pos[0,::skip],pos[1,::skip],'o',label=name)	
+		else:
+			plt.plot(pos[0,::skip],pos[1,::skip],label=name)
 	
 	ax.set_xlabel('x [AU]')
 	ax.set_ylabel('y [AU]')
 	ax.set_zlabel('z [AU]')
 	plt.legend()
 	ax.set_zlim(-1,1)
+	plt.savefig(plot_filename)
 	
 	plt.show()
