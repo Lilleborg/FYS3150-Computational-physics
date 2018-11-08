e7d79d7a772195ed866fab9ca5eb2a643c1f7c6c
wanjapaulsen <wanjap@student.matnat.uio.no>
2018-11-08 14:05:12 +0100

    Cleaned up warnings in the code related to int and uword

--- Projects/Project4/magnetic_systems/main.cpp
+++ Projects/Project4/magnetic_systems/main.cpp
@@ -7,110 +7,90 @@ using namespace arma;
 
 
 void print(std::vector<double> const &input);
-void Initialize(mat &Lattice, int Nspins);
-void EnergyCalculation(mat &Lattice, double &E, int Nspins);
-void MetropolisAlgorithm(int Nspins, mat &Lattice, int MC, vector<double> &Energy, double T, vector<double> &MagneticMom);
+void EnergyCalculation(const mat &Lattice, double &E, uword Nspins);
+void MetropolisAlgorithm(uword Nspins, mat &Lattice, int MC, vector<double> &Energy, double T, vector<double> &MagneticMom);
 
-inline int PeriodicBoundary(int i, int limit, int add) { //Theft
+inline uword PeriodicBoundary(uword i, uword limit, uword add) { //Theft
   return (i+limit+add) % (limit);
 }
 
 int main()
 {
-    int Nspins = 2;
+    uword Nspins = 2;
     double T = 1.0 ;
     int MC = 1000;
     vector<double> Energy;
     vector<double> MagneticMom;
-    mat Lattice(Nspins, Nspins, fill::zeros);
+    mat Lattice(Nspins, Nspins, fill::ones);
 
     MetropolisAlgorithm(Nspins, Lattice, MC, Energy, T, MagneticMom);
 
-    /*
-    cout << "Energy = " << endl;
-    print(Energy);
-    cout << " " << endl;
-    cout << "Magnetic Moment = " << endl;
-    print(MagneticMom);
-    cout << " " << endl;
-    */
-
-    int sum_of_elems = 0;
-    std::for_each(Energy.begin(), Energy.end(), [&] (int n) {
+    uword sum_of_elems = 0;
+    std::for_each(Energy.begin(), Energy.end(), [&] (uword n) {
         sum_of_elems += n;
     });
-    cout << (double)sum_of_elems/(double) MC << endl;
+    cout << double (sum_of_elems) / double (MC) << endl;
     return 0;
 }
 
-void Initialize(mat &Lattice, int Nspins)
-{
-    //Fill initial matrix and initial magnetic moment
-    for (int i = 0; i<Nspins; i++){
-        for (int j = 0; j<Nspins; j++){
-            Lattice(i,j) = 1.0; //Spin up for ground state
-        }
-    }
-}
-void EnergyCalculation(mat &Lattice, double &E, int Nspins)
+void EnergyCalculation(const mat &Lattice, double &E, uword Nspins)
 {
     //Calculate energy of whole lattice
-    for (int i = 0; i<Nspins; i++){
-        for (int j = 0; j<Nspins; j++){
-            E -= (double) Lattice(i,j)*( Lattice(PeriodicBoundary(i, Nspins, -1), j))
-                    + Lattice(i, PeriodicBoundary(j, Nspins, -1));
+    for (uword i = 0; i<Nspins; i++){
+        for (uword j = 0; j<Nspins; j++){
+            E -= double (Lattice(i,j)*( Lattice(PeriodicBoundary(i, Nspins, 1), j))
+                    + Lattice(i, PeriodicBoundary(j, Nspins, 1)));
         }
     }
 }
 
-void MetropolisAlgorithm(int Nspins, mat &Lattice, int MC, vector<double> &Energy, double T, vector<double> &MagneticMom)
+void MetropolisAlgorithm(uword Nspins, mat &Lattice, int MC, vector<double> &Energy, double T, vector<double> &MagneticMom)
 {
     std::random_device rd;
     std::mt19937_64 gen(rd());
     std::uniform_real_distribution<double>RandomGenerator(0.0, 1.0);
-    double E0 = 0;
-    double E = 0;
-    double w = 0;
-
-    Initialize(Lattice, Nspins); //Initialize lattice
-    //cout << Lattice << endl;
+    double E0;
+    double E;
+    double w;
 
     for (int i=0; i<MC; i++){
         E0 = 0.0;
         E = 0.0;
         w = 0.0;
-        //cout << i << endl;
+
         EnergyCalculation(Lattice, E0, Nspins); //Calculate initial energy
         cout << E0 << endl;
         //Choose random position in lattice and flip spin
-        int x = (int) (RandomGenerator(gen)*Nspins);
-        int y = (int) (RandomGenerator(gen)*Nspins);
+        uword x = uword ((RandomGenerator(gen)*Nspins));
+        uword y = uword ((RandomGenerator(gen)*Nspins));
         Lattice(x, y) *= -1;
         cout << Lattice << endl;
         //Calculate new energy
         E = E0;
         EnergyCalculation(Lattice, E, Nspins);
         if (E-E0 < 0.0){ //Keep energy if new one is lower
-            Energy.push_back((double) E0);
-            MagneticMom.push_back((double) 2*Lattice(x,y));
+            Energy.push_back(double (E0));
+            MagneticMom.push_back(double (2*Lattice(x,y)));
         }
         else{ //If energy is larger
             w = exp(-1*(E-E0)/T);
-            if (w >= (double) RandomGenerator(gen)){ //If true, flip spin back again
+            if (w >= double (RandomGenerator(gen))){ //If true, flip spin back again
                 Lattice(x, y) *= -1;
             }
             else{ //Keep energy if not true
-                Energy.push_back((double) E0);
-                MagneticMom.push_back((double) 2*Lattice(x,y));
+                Energy.push_back(double (E0));
+                MagneticMom.push_back(double (2*Lattice(x,y)));
             }
         }
 
     }
 }
 
+
+//USED FOR PRINTING TYPE VECTOR IF NEEDED
 void print(std::vector<double> const &input) //Theft
 {
-    for (int i = 0; i < input.size(); i++) {
+    for (uword i = 0; i < input.size(); i++) {
         std::cout << input.at(i) << ' ';
     }
 }
