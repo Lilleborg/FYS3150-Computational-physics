4f0c3687c53d8d890740a1462ff02611e604b91b
Jakob Borg <jakobbor@student.matnat.uio.no>
2018-12-01 23:05:20 +0100

    Project pdf file, commiting main before push

--- Projects/Project5/sicc/main.cpp
+++ Projects/Project5/sicc/main.cpp
@@ -5,6 +5,25 @@ using namespace std;
 
 int main()
 {
+    // Oppgave A
+
+
+
+    // Initialize populations
+    vector<double> as = {4,4,4,4};
+    vector<double> bs = {1,2,3,4};
+    vector<double> cs = {0.5,0.5,0.5,0.5};
+    vector<Populations> m_populations;
+    for (int i = 0; i < 4; ++i) {
+        m_populations
+    }
+    Popa = Populations()
+
+
+
+
+
+
     cout << "Hello World!" << endl;
     return 0;
 }
