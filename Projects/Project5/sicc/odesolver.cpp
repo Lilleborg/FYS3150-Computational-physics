3686129df2c77114c31004368021801255c72e19
Jakob Borg <jakobbor@student.matnat.uio.no>
2018-12-03 22:40:17 +0100

    rk4 functionality untested

--- Projects/Project5/sicc/odesolver.cpp
+++ Projects/Project5/sicc/odesolver.cpp
@@ -25,18 +25,18 @@ vector<double> ODEsolver::vec_pluss(const vector<double> vec, double pluss)
 //{
 //}
 
-void ODEsolver::RK4(population_group *p,double t)
+void ODEsolver::RK4(population_group *p, double t)
 {
-
-
+    get_ks(p,t);
+    p->add_to_current(1.0/6*(ks[0]+2*ks[1]+2*ks[2]+ks[1]));
 }
 
-void ODEsolver::get_ks(population_group *p,double t)
+void ODEsolver::get_ks(population_group *p, double t)
 {
     ks.clear();
-    ks[0] = m_dt*p->prime(m_problem->m_Qs,t);
-    ks[1] = m_dt*p->prime(vec_pluss(m_problem->m_Qs, ks[0]/2.0),t + m_dt/2.);
-    ks[2] = m_dt*p->prime(vec_pluss(m_problem->m_Qs, ks[1]/2.0),t + m_dt/2.);
-    ks[3] = m_dt*p->prime(vec_pluss(m_problem->m_Qs, ks[2]),t + m_dt);
+    ks[0] = m_dt*p->prime(m_problem->m_Qs, t);
+    ks[1] = m_dt*p->prime(vec_pluss(m_problem->m_Qs, ks[0]/2.0), t + m_dt/2.);
+    ks[2] = m_dt*p->prime(vec_pluss(m_problem->m_Qs, ks[1]/2.0), t + m_dt/2.);
+    ks[3] = m_dt*p->prime(vec_pluss(m_problem->m_Qs, ks[2]), t + m_dt);
 }
 
