cb7d5c2eeeb1505f5b144e804545f08d986cf3c7
Jakob Borg <jakobbor@student.matnat.uio.no>
2018-12-05 21:38:31 +0100

    Large update to problem class. Involved new writeme class, discription:
    Added methods for writing to file with writeme class, + configurating writeme
    Added map of parameters.
    Added evolve_full method solving the problem to final time with testing.

--- Projects/Project5/sicc/problem.cpp
+++ Projects/Project5/sicc/problem.cpp
@@ -1,8 +1,9 @@
 #include "problem.h"
 
-problem::problem(int N, int S0, int I0, int R0)
+problem::problem(int N, int S0, int I0, string name_, int R0)
 {
     m_N = N; m_S0 = S0; m_I0 = I0; m_R0 = R0;
+    name = name_;
 }
 
 problem::~problem()
@@ -17,6 +18,12 @@ problem::~problem()
 void problem::set_parameters(double b, double c, double d, double dI, double e, double f)
 {
     m_b = b; m_c = c; m_d = d; m_dI = dI; m_e = e; m_f = f;
+    m_params["b"] = m_b;
+    m_params["c"] = m_c;
+    if (m_d != 0) m_params["d"] = m_d;
+    if (m_dI != 0) m_params["dI"] = m_dI;
+    if (m_e != 0) m_params["e"] = m_e;
+    if (m_f != 0) m_params["f"] = m_f;
 }
 
 void problem::set_timing(double dt, double T, double T0)
@@ -29,6 +36,7 @@ void problem::set_timing(double dt, double T, double T0)
         time.push_back(i);
     }
     solver = new ODEsolver(this,m_dt);
+    current_step = 0;
 }
 
 void problem::set_population()
@@ -38,6 +46,7 @@ void problem::set_population()
     SIR.push_back(new Infected(m_N,m_I0,m_afunc,m_b,m_d,m_dI));
     SIR.push_back(new Recovered(m_N,m_R0,m_b,m_c,m_d,m_f));
     this->update_current();
+    current_step = 0;
 }
 
 void problem::update_current()
@@ -46,21 +55,115 @@ void problem::update_current()
     {
         m_Qs[i] = SIR[i]->get_current();
     }
+    update_all();
 }
 
-void problem::evolve()
+void problem::update_all()
 {
-    //for (population_group Q : SIR)
+    S_vector.push_back(m_Qs[0]);
+    I_vector.push_back(m_Qs[1]);
+    R_vector.push_back(m_Qs[2]);
+}
+
+void problem::evolve(double t)
+{
     for (int i = 0; i<3; i++)
     {
-        solver->RK4(SIR[i],time[0]);
+        solver->RK4(SIR[i],t);
     }
     update_current();
+    current_step += 1;
 }
 
-void problem::print_current_SIR()
+void problem::evolve(int i)
 {
-   std::cout << "S: " << m_Qs[0] << " , I: " << m_Qs[1] << " , R: " << m_Qs[2] << std::endl;
+    evolve(time[i]);
+}
+
+bool problem::test_total_population()
+{
+    test_sum = 0;
+    double tol = 5;
+    for (double& q : m_Qs)
+    {
+        test_sum += q;
+    }
+    return fabs(test_sum-m_N)>tol;
+}
+
+void problem::evolve_full(bool debug)
+{
+    cout << "Running full simulation of problem " << name << " with " << m_nr_steps << " steps\n";
+    cout << "..." << endl;
+    clock_t timestart = clock();
+    for (int i = 0; i < m_nr_steps;i++)
+    {
+        this->evolve(i);
+        if (debug)
+        {
+            if (i%int(m_nr_steps/1000) == 0)
+            {
+                if (this->test_total_population())
+                {
+                    cout << "-------------------------------------------------------------------\n";
+                    cout << "Terminating run as total number of people has changed significantly\n";
+                    cout << "-------------------------------------------------------------------\n";
+                    cout << "Total population initially" << m_N << ", current population " << test_sum << "\n";
+                    cout << "at time " << time[i] << " ran " << i << " steps." << endl;
+                    break;
+                }
+            }
+        }
+    }
+    cout << "Simulation done in " << (double(clock()-timestart)/double(CLOCKS_PER_SEC)) << " seconds.\n" << endl;
+}
+
+void problem::config_write(writeme *write,string filenamestart)
+{
+    write->clear_default_filename();
+    write->clear_subpath();
+    write->set_subpath(name + "/");
+    for (const auto& e : m_params)
+    {
+        write->add_double(e.first,e.second);
+    }
+    write->add_int("N", m_N);
+    write->set_default_filename(filenamestart + "_" + m_afunc->get_name(),true,true);
+}
+
+void problem::write_SIR(writeme *write, bool config)
+{
+    if (config)
+    {
+        config_write(write);
+    }
+    vector<vector<double>> temp;
+    temp.push_back(S_vector);
+    temp.push_back(I_vector);
+    temp.push_back(R_vector);
+    write->write_vector_vector(temp);
+}
+
+void problem::write_SIR_one_by_one(writeme *write, bool config)
+{
+    if (config)
+    {
+        config_write(write,"");
+    }
+    write->write_double_vector(S_vector,"S");
+    write->write_double_vector(I_vector,"I");
+    write->write_double_vector(R_vector,"R");
+}
+
+void problem::print_current_SIR(bool debug)
+{
+    if (debug)
+    {
+        this->test_total_population();
+        cout << "At time " << time[current_step] << " index " << current_step << " the sum of current SIR values " << test_sum;
+        cout << " vs initial " << m_N << endl;
+    }
+    cout << "S: " << m_Qs[0] << " , I: " << m_Qs[1] << " , R: " << m_Qs[2] << endl;
 }
 
 vector <population_group*> problem::get_SIR()
--- Projects/Project5/sicc/problem.h
+++ Projects/Project5/sicc/problem.h
@@ -3,23 +3,26 @@
 
 #include <iostream>
 #include <math.h>
+#include "time.h"
 
 #include "population/a_parameter.h"
+#include "writeme.h"
 #include "odesolver.h"
 #include "population/population_group.h"
 #include "population/susceptibles.h"
 #include "population/infected.h"
 #include "population/recovered.h"
 
+using namespace std;
+
 // TODO: (?)
-//vector <double> m_parameters; list of all parameters for printing current params for this problem
-// add write to file functionality
+//printing current params for this problem
 
 class problem
 {
 public:
     // Initiallizers and setters
-    problem(int N, int S0,int I0, int R0 = 0);
+    problem(int N, int S0,int I0,string name_ = "", int R0 = 0);
     ~problem();
 
     void set_parameters(double b, double c, double d=0, double dI=0, double e=0, double f=0);//init param
@@ -28,26 +31,30 @@ public:
     void set_population();  // Initialize population classes
 
     // Calculations
-    void evolve();  // evolve SIR systems 1 step
+    void evolve(double t);  // evolve SIR systems 1 step
+    void evolve(int i); // evolve using index
     void update_current();  // update local current values of current SIR
+    void evolve_full(bool debug = false); // run full simulation untill time[i] = T, with test of total population if test = true
+    void update_all();  // add current values to SIR vectors
+    bool test_total_population(); double test_sum;
 
     // Convinient functions
-    void print_current_SIR();
+    void print_current_SIR(bool debug = false);
     vector <population_group*> get_SIR();
 
     // Parameters, initial conditions
+    string name;
     double m_b,m_c,m_d,m_dI,m_e,m_f;
+    map <string,double> m_params;
     int m_N,m_S0,m_I0,m_R0;
 
     // Timestuff
     double m_dt,m_T,m_T0;
-    int m_nr_steps;
+    int m_nr_steps,current_step;
     vector <double> time;
 
     // Vectors holding current SIR values
     vector <double> m_Qs = vector<double>(3,0);
-
-    // Vectors holding all SIR values for writing
     vector <double> S_vector;
     vector <double> I_vector;
     vector <double> R_vector;
@@ -60,6 +67,12 @@ public:
 
     // ODEsolver object, initialized by void set_timing()
     ODEsolver *solver = nullptr;
+
+    // Writing to file
+    void config_write(writeme *write, string filenamestart = "SIR");
+    void write_SIR(writeme *write, bool config = true);
+    void write_SIR_one_by_one(writeme *write, bool config = true);
+
 };
 
 #endif // PROBLEM_H
