d7d09d94efaf8658107a724f7cd63ad181059d1d
Jakob Borg <jakobbor@student.matnat.uio.no>
2018-12-06 22:34:57 +0100

    solving exe A with set of params

--- Projects/Project5/sicc/main.cpp
+++ Projects/Project5/sicc/main.cpp
@@ -11,42 +11,28 @@ using namespace std;
 int main()
 {
     // Constants
-    double b = 2; double c = 0.5;
+    double b = 1; double c = 0.5;
 
     // a-parameters
-    a_constant *a_c = new a_constant(3);
-    a_seasons *a_s = new a_seasons(4,1,M_PI);
+    a_constant *a_c = new a_constant(4);
+    a_seasons *a_s = new a_seasons(4,2,1/2.0*M_PI);
 
     // writeme object
     writeme *write = new writeme("./../datafiles/");
 
-    // Testing implementation
-    bool debug = false;
+    // Exercise A
+    bool debug = true;
     problem A(400,300,100,"exeA");
     A.set_afunc(a_s);
-    A.set_parameters(b,c);
-    A.set_population();
-    A.set_timing(0.001,200);
-
-    A.evolve_full(debug);
-    //A.write_SIR_one_by_one(write);
-    A.write_SIR(write);
-    A.print_current_SIR(debug);
-
-
-
-
-    // test writeme
-    //    vector <vector<double>> testvec {{1,2,3},{4,5,6},{7,8,9}};
-    //    cout << testvec.size() << endl;
-    //    string path = "./../datafiles/";
-
-    //    writeme writetest2(path, "test1");
-    //    writetest2.add_double("a",4);
-    //    writetest2.add_int("T",100);
-
-    //    writetest2.write_vector_vector(testvec);
-
-    cout << "Hello World!" << endl;
+    A.set_timing(0.0001,20);
+    for (double b = 1; b <= 4; b++)
+    {
+        A.set_parameters(b,c);
+        A.set_population();
+        A.evolve_full(debug);
+        //A.write_SIR(write);
+        A.write_SIR_bin(write);
+    }
+    cout << "Main done!" << endl;
     return 0;
 }
