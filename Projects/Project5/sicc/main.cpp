c7eb45ab7d6f461cd7eb5c9d97b360fd4fdc8da8
Jakob Borg <jakobbor@student.matnat.uio.no>
2018-12-11 14:22:57 +0100

    some intro to code.

--- Projects/Project5/sicc/main.cpp
+++ Projects/Project5/sicc/main.cpp
@@ -10,14 +10,31 @@
 
 using namespace std;
 
+/* Brief code intro:
+ * This main function use the class 'problem' to set up each problem we are looking at with different
+ * parameters and timing.
+ * A set of classes and common base classes are used. In the directory 'population' there are two
+ * common bases 'population_group' and 'a_parameter'. a_constant' and 'a_seasons' is determined outside the 'problem'
+ * and sendt in as a parameter just like the other parameters.
+ *
+ * The different population groups S, I and R shares common base 'population_group' and is initialized and handled
+ * by the 'problem'.
+ *
+ * 'writeme' class is a work in progress for general c++ projects, intended to extend with more write functions.
+ * Here it is used to write the different files we want to plot, it automaticly handles the different filenames and
+ * directory paths set by the 'problem'.
+ *
+ * The RK4 solver was implemented first together with the 'problem' class, and is initialized and controlled by
+ * the 'problem'. The MC solver was implemented last of all, at which point I didnt want to change too much in
+ * 'problem'. In addition I think it's better that the 'solver' takes the problem as input, and not the other way around.
+ * In hinsight I would like it if the RK4 solver operated in the same way as the MC solver.
+
+*/
 /* TODO:
  * comment throughout code
- * MPI? Set up own main.cpp for this?
- *
- *
+ * MPI? Set up own main.cpp for this? No need for MPI as the solvers are so fast.
  *
 */
-
 
 int main(int numArguments, char **arguments)
 {
@@ -41,10 +58,10 @@ int main(int numArguments, char **arguments)
     {
         if (strcmp(arguments[1],"A") == 0 || strcmp(arguments[1],"ALL") == 0)   // RUN EXERCISE A and B
         {
-            problem A(400,300,100,"exeAandB/c_0.5");
+            problem A(400,300,100,"exeAandB/c_0.5_test");
             A.set_afunc(a_c);
-            A.set_timing(0.001,10);
-            for (double b = 1; b <= 4; b++)
+            A.set_timing(0.1,10);
+            for (double b = 1; b <= 1; b++)
             {
                 // RK4
                 A.set_parameters(b,c);
@@ -53,17 +70,18 @@ int main(int numArguments, char **arguments)
                 A.write_SIR_bin(write);
 
                 // MC
-                A.set_population();
-                MonteCarlo MC_obj(&A,100);
-                MC_obj.Run_MC_const_population();
-                MC_obj.write_averages(write);
+//                A.set_population();
+//                MonteCarlo MC_obj(&A,100);
+//                MC_obj.Run_MC_const_population();
+//                MC_obj.write_averages(write);
             }
+            cout <<"A time end" << " " <<  A.time.back() << endl;
 
         }   // IF exe A AND B END
 
         if (strcmp(arguments[1],"C") == 0 || strcmp(arguments[1],"ALL") == 0)   // RUN EXERCISE C
         {
-            problem C(400,300,100,"exeC/c_0.5");
+            problem C(400,300,100,"exeC/c_0.5_vary_dty");
             C.set_afunc(a_c);
             C.set_timing(0.001,40);
             for (double b = 1; b <= 4; b++)
@@ -110,7 +128,7 @@ int main(int numArguments, char **arguments)
         {
             problem E(400,300,100,"exeE/c_0.5");
             E.set_afunc(a_s);
-            E.set_timing(0.001,20);
+            E.set_timing(0.0001,20);
             for (double b = 1; b <= 4; b++)
             {
                 // RK4
